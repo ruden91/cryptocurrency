3b95dfaeedb9b83ddb411636ac72e8ee6f7dae8e
ruden91 <ruden91@naver.com>
2018-06-18 01:31:36 +0900

    localStorage 적용

--- data/index.js
+++ data/index.js
@@ -2,8 +2,8 @@ const database = require('./firebase').database;
 const fs = require('fs');
 
 fs.readFile('./json/coinhills.json', (err, data) => {
-  database.ref('exchangeRanking').set(JSON.parse(data));
-
-  console.log('데이터가 정상적으로 올라갔습니다.');
-  // process.exit();
+  database.ref('exchangeRanking').set(JSON.parse(data), () => {
+    console.log('데이터가 정상적으로 올라갔습니다.');
+    process.exit();
+  });
 });
